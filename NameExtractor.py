4da07fac062c7a79f558160397486d0f8dd61cd7
dbhimjiyani <dbhimjiyani@users.noreply.github.com>
2019-06-10 11:32:00 -1000

    TrainTorch now displays 4 random images that have been transformed along with their class labels

--- NameExtractor.py
+++ NameExtractor.py
@@ -1,54 +1,89 @@
-from pathlib import Path # Used to change filepaths
 import os
-import glob
-import csv
 import re
-# Check the directory. Just so we know exactly where we is u kno
-print(os.getcwd())
+import skimage
 
-# PLEASE MAKE SURE YOU ARE LINKING TO THE RIGHT DIRECTORY THANK YOU
-# A List of images here, we're making it dynamically by grabbing it from a folder called photo
-image_paths = [Path(item) for i in [glob.glob(r'data\common\*.%s' % ext) for ext in ["jpg", "jpeg", "tiff", "tif"]] for item in i]
-# --------------- look above should you change the csv from the default
+import pandas as pd
+from torch.utils import data
 
 
-def createCSV(path, writer):
-    #text = re.sub(r'data\\train\\', '', str(path))
-    text = (path.stem)
-    # Regex for extracting details from name of the file, such as the species type, code and the angle of shot
-    matchObj = re.match(
-        r'([a-zA-Z0-9]+)_(smeared|contrasted)*_*([a-zA-Z0-9]+)_([a-zA-Z0-9]+)', text) # <--------------- look here should you change the csv from the default
-        # check what this regex is doing by going on https://regex101.com/ & testing against random filenames from your folder
+def parse_image_names(image_directory="data", csv_file="data/data.csv", write_csv=True): # <----- look here should you change the csv from the default
+    data = []
+    image_paths = [fn for fn in os.listdir(image_directory)
+                   if os.path.splitext(fn)[-1].lower() in {".jpg", ".jpeg", ".tiff", ".tif"}]
 
-    try:
-        species = matchObj.group(1)  # Species # <--------------- look here should you change the csv from the default
-        bfId = matchObj.group(3)  # UASMXXXX
-        view = matchObj.group(4)  # self-explanatory
-    except AttributeError:
-        species = "Error_Species"
-        bfId = "Error"
-        view = "Error"
+    labels = ["sample_id", "species", "seasonal_variant", "view", "filename"]  # <----- look here should you change the csv from the default
+    data = []
 
-        # Had to do seasonal separately as the value may be null if the butterflly has had no seasonal variation
-    if(matchObj.group(2) is None):
-        seasonal = ""
-    else:
-        seasonal = matchObj.group(2)  # contrasted? smeared? what it iz? (seasonal variations)
+    for filename in image_paths:
 
-    writer.writerow([bfId, species, seasonal, view, path]) # write the newly assigned variables to the file
-    print(species + "  " + seasonal + "     " + text) # print formatting no biggie
+        match_obj = re.match(r'([a-zA-Z0-9]+)_(smeared|contrasted)*_*([a-zA-Z0-9]+)_([a-zA-Z0-9]+)',  # <---- look here should you change the csv from the default
+                             os.path.splitext(filename)[0])
+        if match_obj is None:
+            # todo: emit log entry
+            pass
+
+        # these should exist in all data <----- look here should you change the csv from the default
+        species = match_obj.group(1)
+        sample_id = match_obj.group(3)
+        view = match_obj.group(4)
+
+        # some entries have no seasonal variation
+        if match_obj.group(2) is not None:
+            seasonal_variant = match_obj.group(2)  # seasonal variations {contrasted, smeared}
+        else:
+            seasonal_variant = ""
+
+        data.append((sample_id, species, seasonal_variant, view, filename))
+
+    sample_df = pd.DataFrame.from_records(data, columns=labels, index="sample_id")
+
+    # assign integers to each unique species, ordered alphabetically, and create dict
+    int_key_dict = {species: sp_int for sp_int, species in enumerate(sorted(sample_df['species'].unique()))}
+
+    # append this new mapping on to the dataframe, so we have the corresponding int value col in the DF now
+    sp_ints = sample_df["species"].apply(lambda sp: int_key_dict[sp])
+
+    highest_int = len(int_key_dict) - 1
+    sample_df["sp_float"] = sp_ints.apply(lambda sp_int: sp_int / highest_int)
+
+    if write_csv:
+        sample_df.to_csv(csv_file)
+
+    return sample_df
+
+# This dataset class can now be imported into the rest of the scripts in the software -- yey!
+class ButterflyDataset(data.Dataset):
+    # make the dataset to point to our image collection
+
+    def __init__(self, csv_file="data/data.csv", image_dir="data", transform=None):
+
+        # if os.path.isfile(csv_file):
+        #     self.sample_df = pd.read_csv(csv_file, index_col=0)
+        # else:
+        self.sample_df = parse_image_names(image_dir, csv_file)
+
+        # transform is optional and can be called on a sample if like
+        self.transform = transform
+        self.image_dir = image_dir
+
+    def __len__(self):
+        return len(self.sample_df)
+
+    def __getitem__(self, idx):
+        filename = self.sample_df.iloc[idx, 3]
+        path = os.path.join(self.image_dir, filename)
+
+        image = skimage.io.imread(path)
+        sp_float = self.sample_df.iloc[idx, 4]
+
+        sample = (sp_float, image)
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
 
-with open('data/common/Data.csv', mode='w') as bfs: # <--------------- look here should you change the csv from the default
-    # opening the csv file and assigning a writer
-    bfs_writer = csv.writer(bfs, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    # writing row
-    bfs_writer.writerow(['Butterfly ID', 'Species', 'Seasonal Variations', 'View', 'FileName'])
-    # for loop over image paths so we do the following all images are in the csv
-    for img_path in image_paths:
-        # call the function written above
-        createCSV(Path(img_path), bfs_writer)
-
-print("youe foole. creating the csv my damn self because nobody helps me in this house")
-
-
+if __name__ == "__main__":
+    # quick test
+    print(next(iter(ButterflyDataset("data/common/Data.csv", image_dir="data/common"))))
--- README.md
+++ README.md
@@ -17,6 +17,6 @@
 
 
 ## Modifying DINNGuS for Your Own Use
-* Name Extractor: In order to use your own Dataset, you will need to modify the NameExtractor script which extracts vital data from the the filenames and sorts them in to a categories within a Pandas DataFrame. This makes use of RegEx extraction of the species name, insect ID, view of the critter and file name. This RegEx can be modified at https://regex101.com/r/rKpb3M/1
+* Name Extractor: In order to use your own Dataset, you will need to modify the NameExtractor script which extracts vital data from the filenames and sorts them in to a categories within a Pandas DataFrame. This makes use of RegEx extraction of the species name, insect ID, view of the critter and file name. This RegEx can be modified at https://regex101.com/r/rKpb3M/1
 
 ## About the Devs
